cbd05db48ef27bdd00647dad3b287a06152b334e
Ruslan Shupoval <ruslan.shupoval@advancedmd.com>
2018-06-07 16:47:38 -0600

    added demo cmd app

---
+++ SourceLink.Git.GitHubDemo.Cmd/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SourceLink.Git.GitHubDemo.Cmd {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+      Console.WriteLine(new Class1().Get());
+
+    }
+  }
+}
